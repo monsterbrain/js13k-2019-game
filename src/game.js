5998d6df3d75d57f66a0b527b577d27a6c859905
Faisal rasak <monsterkillu@gmail.com>
2019-09-12 22:40:11 -0700

    Update game.js
    added joystick controls and keyboard as well

--- src/game.js
+++ src/game.js
@@ -1,33 +1,35 @@
+// v 0.5 updated 12-10-19
 (function () {
     var requestAnimationFrame = window.requestAnimationFrame || window.mozRequestAnimationFrame || window.webkitRequestAnimationFrame || window.msRequestAnimationFrame;
     window.requestAnimationFrame = requestAnimationFrame;
 })();
-
+ 
 var canvas = document.getElementById("gameCanvas");
 var ctx = canvas.getContext('2d');
 var keys = [];
-
+ 
 var storedPosArray = [];
-
+ 
 var frameTimer = 0.0;
 var keyFrameStepDuration = 200; //ms
-
+ 
 const LEFT = 0, RIGHT = 1, UP = 2, DOWN = 3, UPDOWN = 4, LEFTRIGHT = 5;
-
+ 
 var gameWidth = 720, gameHeight = 480;
-
+ 
 var player = {
     x: gameWidth / 2,
     y: 200,
     width: 25,
     height: 25,
+    dir: UP,
     speed: 3,
     velX: 0,
     velY: 0,
     accel: 2,
     color:'#3388FF'
 }
-
+ 
 var enemy = {
     x: gameWidth / 4,
     y: 200,
@@ -43,35 +45,58 @@ var enemy = {
     patrolTimer: 0,
     patrolDuration: 2000 //ms
 }
-
+ 
 class Joystick {
-  cx=0; cy=0;
   constructor(x,y,cr){
-    this.cx = x 
-    this.cy = y 
+    this.inx = x
+    this.iny = y
+    this.cx = x
+    this.cy = y
     this.cr = cr
+    this.inR = 10
   }
-  
+ 
   getDir(mx, my){
     var dist = (this.cx-mx)*(this.cx-mx)+(this.cy-my)*(this.cy-my)
     if(dist<=(this.cr*this.cr)){
       console.log("inside touch")
-      var ang = Math.atan2(my-this.cy, mx-this.cx)
+      var ang = Math.atan2(my-this.cy, mx-this.cx)*180/Math.PI
       console.log("ang = "+ang)
+
+      this.inx = mx
+      this.iny = my
+     
+      if(ang<45 && ang>-45)
+          return RIGHT
+      else if(ang>-135 && ang <-45)
+          return UP
+      else if(ang<135 && ang>=45)
+          return DOWN
+      else
+          return LEFT
     }
+    return -1
   }
-  
+
+  reset(){
+    this.inx = this.cx
+    this.iny = this.cy
+  }
+ 
   draw(ctx){
-    ctx.fillStyle = "#66888888";
+      ctx.beginPath();
+    ctx.fillStyle = "#888888";
     ctx.arc(this.cx, this.cy, this.cr, 0, 2 * Math.PI, false);
+    ctx.fill();
+    // inner joystick
+    ctx.beginPath();
+    ctx.fillStyle = "#333333";
+    ctx.arc(this.inx, this.iny, this.inR, 0, 2 * Math.PI, false);
     ctx.fill();
   }
 }
-
+ 
 class Enemy {
-    patrolTimer = 0
-    patrolDuration = 2000
-    patrolDir = UPDOWN
     constructor(x, y, w, h, speed, dir, color){
         this.x = x
         this.y = y
@@ -80,8 +105,11 @@ class Enemy {
         this.speed = speed
         this.dir = dir
         this.color = color
+        this.patrolTimer = 0
+        this.patrolDuration = 2000
+        this.patrolDir = UPDOWN
     }
-
+ 
     update(dt){
         if(dt){
             let disp = this.speed*(dt/1000)
@@ -90,7 +118,7 @@ class Enemy {
             } else {
                 this.x -= (this.dir == LEFT? disp: -disp)
             }
-            
+           
             this.patrolTimer += dt
             if(this.patrolTimer>this.patrolDuration){
                 this.patrolTimer -= this.patrolDuration;
@@ -102,48 +130,48 @@ class Enemy {
             }
         }
     }
-
+ 
     render(ctx){
         ctx.fill();
         ctx.fillStyle = this.color;
         ctx.fillRect(this.x, this.y, this.w, this.h);
     }
 }
-
+ 
 canvas.width = gameWidth;
 canvas.height = gameHeight;
-
+ 
 ctx.fillStyle = 'Black';
 ctx.fillRect(30, 20, 40, 40);
 ctx.fillRect(110, 20, 40, 40);
 ctx.fillRect(60, 180, 80, 80);
-
+ 
 var isStoringActive = true;
 var isRewindActive = false;
-
+ 
 var rewindStep = 0;
 var prevPos, nextPos;
-
+ 
 var enemyTest = new Enemy(100,200, 25, 25, 200, UP, '#ff4433')
 var joystick = new Joystick(600, 360, 120)
-
+ 
 function tweenPos(dt, duration) {
     var pos = {}
     pos.x = startPos.x + (endPos.x - startPos.x)*(dt/duration)
     pos.y = startPos.y + (endPos.y - startPos.y)*(dt/duration)
     return pos
 }
-
+ 
 var lastTime, deltaTime=0;
 function update(currentTime) {
-
+ 
     if(lastTime==undefined) {
         lastTime=currentTime;
     }
-
+ 
     deltaTime=(currentTime-lastTime);
     lastTime=currentTime;
-
+ 
     if(deltaTime) {
         if(isStoringActive) {
             frameTimer += deltaTime;
@@ -172,9 +200,9 @@ function update(currentTime) {
             }
         }
     }
-    
-    
-
+   
+   
+ 
     var cheatRewind = true;
     // check keys
     if (cheatRewind && !isRewindActive && keys[32]) {
@@ -185,80 +213,102 @@ function update(currentTime) {
         startPos = storedPosArray[rewindStep]
         endPos = storedPosArray[rewindStep-1]
     }
-
+ 
+    var anyKeyDown = false
     if (keys[38] || keys[87]) {
         // up arrow or w
-        if (player.velY > -player.speed) {
-            player.velY -= player.accel;
-        }
+        player.dir = UP
     }
     if (keys[40] || keys[83]) {
         // down arrow or S
-        if (player.velY < player.speed) {
-            player.velY+=player.accel;
-        }
+        player.dir = DOWN
     }
     if (keys[39] || keys[68]) {
         // right arrow
-        if (player.velX < player.speed) {
-            player.velX+= player.accel;
-        }
+        player.dir = RIGHT
     }
     if (keys[37] || keys[65]) {
         // left arrow
+        player.dir = LEFT
+    }
+   
+    if(player.dir == UP){
+        if (player.velY > -player.speed) {
+            player.velY -= player.accel;
+        }
+    } else if(player.dir == RIGHT){
+        if (player.velX < player.speed) {
+            player.velX+= player.accel;
+        }
+    } else if(player.dir == DOWN){
+        if (player.velY < player.speed) {
+            player.velY+=player.accel;
+        }
+    } else if(player.dir == LEFT){
         if (player.velX > -player.speed) {
             player.velX-=player.accel;
         }
     }
-    
+
+    if(!isMouseDown)
+        player.dir = -1 // reset player movement
+   
     ctx.clearRect(0, 0, gameWidth, gameHeight);
     ctx.beginPath();
-
+ 
     player.x += player.velX;
     player.y += player.velY;
-
+ 
     player.velX = player.velY = 0
-
+ 
     ctx.fill();//Draw charater stuff
     ctx.fillStyle = player.color;
     ctx.fillRect(player.x, player.y, player.width, player.height);
-
+ 
     enemyTest.update(deltaTime)
     enemyTest.render(ctx)
-    
+   
     joystick.draw(ctx)
-
+ 
     // draw enemy stuff
     ctx.fillStyle = enemy.color;
     ctx.fillRect(enemy.x, enemy.y, enemy.width, enemy.height);
-
+ 
     if(collides(player, enemy)){
         console.log("enemy hit");
     }
-
+ 
     requestAnimationFrame(update);
 }
-
+ 
 // resizing and loading windows
 window.addEventListener('load', onLoad, false);
 window.addEventListener('resize', resize, false);
-
+ 
 document.body.addEventListener("keydown", function (e) {
     keys[e.keyCode] = true;
 });
-
+ 
 document.body.addEventListener("keyup", function (e) {
     keys[e.keyCode] = false;
 });
-
+ 
+var isMouseDown = false;
 canvas.addEventListener("mousedown",(e)=>{
   var rect = canvas.getBoundingClientRect()
   var pos = getMousePos(canvas, e)
   console.log(pos.x+","+pos.y)
-  joystick.getDir(pos.x, pos.y)
+  player.dir = joystick.getDir(pos.x, pos.y)
+  isMouseDown = true;
 })
 
-
+canvas.addEventListener("mouseup",(e)=>{
+    player.dir = -1
+    isMouseDown = false;
+    joystick.reset()
+})
+ 
+ 
 // ref : http://jeffreythompson.org/collision-detection/rect-rect.php
 function collides(r1, r2) {
     var collided = false;
@@ -272,15 +322,15 @@ function collides(r1, r2) {
     // Is the LEFT edge of r1 to the LEFT of the RIGHT edge of r2?
     // Is the BOTTOM edge of r1 BELOW the TOP edge of r2?
     // Is the TOP edge of r1 ABOVE the BOTTOM edge of r2?
-    
+   
     return collided;
 }
-
+ 
 function onLoad(){
     //resize();
     update();
 }
-
+ 
 function resize() {
     // todo get the width of game area
     // Resize the window, not the pen
@@ -288,18 +338,18 @@ function resize() {
     // regardless of the resolution
     console.log("resize")
     var height = window.innerHeight;
-  
+ 
     // So we need to calculate the proper scaled width
     // that should work well with every resolution
     var ratio = canvas.width / canvas.height;
     var width = height * ratio;
-    
+   
     console.log(width+","+height)
-  
+ 
     canvas.style.width = width + 'px';
     canvas.style.height = height + 'px';
 }
-
+ 
 function getMousePos(canvas, evt) {
   var rect = canvas.getBoundingClientRect();
   return {
